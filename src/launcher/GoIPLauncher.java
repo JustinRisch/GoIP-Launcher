e5ab58ed4c738823594d31dee827fb8bec81546b
Justin Risch <JustinRisch@gmail.com>
2015-02-25 15:21:18 -0500

    fixed single byte version limitation

--- src/launcher/GoIPLauncher.java
+++ src/launcher/GoIPLauncher.java
@@ -3,7 +3,6 @@ package launcher;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -11,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Scanner;
 
 import goip.*;
 
@@ -23,60 +21,61 @@ import javax.swing.JLabel;
 public class GoIPLauncher {
 	static JFrame jd;
 	static JButton updater;
-	static int currentVersion = -1;
+	static double currentVersion = -1;
+	private static final double version = 11;
 
 	public static void main(String[] args) {
 		jd = new JFrame();
 		jd.setResizable(false);
 		jd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		int version = -1;
 		try {
 			currentVersion = getCurrentVersion();
-			version = getVersion();
-			jd.setTitle("GoIP");
+			jd.setTitle("GoIP v"+version/10);
+
+			jd.setBounds(100, 100, 300, 100);
+			jd.setLocationRelativeTo(null);
+			
+			if (currentVersion == version) {
+
+				jd.add(new JLabel("Choose your role:"), BorderLayout.NORTH);
+				JButton DMButton = new JButton("Dungeon Master");
+				DMButton.addActionListener(e -> {
+					try {
+						GoIPDM.main(args);
+						jd.dispose();
+					} catch (Exception d) {
+						jd.removeAll();
+						jd.add(new JLabel(d.getMessage()));
+						jd.revalidate();
+						jd.repaint();
+					}
+				});
+				jd.add(DMButton, BorderLayout.CENTER);
+
+				JButton PlayerButton = new JButton("Player");
+				PlayerButton.addActionListener(e -> {
+					try {
+						GoIPPlayer.main(args);
+						jd.dispose();
+					} catch (Exception d) {
+						jd.removeAll();
+						jd.add(new JLabel(d.getMessage()));
+						jd.revalidate();
+						jd.repaint();
+					}
+				});
+				jd.add(PlayerButton, BorderLayout.SOUTH);
+				jd.setVisible(true);
+			} else {
+				System.out.println(currentVersion);
+				updater = new JButton("Update GoIP to "+ currentVersion/10);
+				updater.addActionListener(GoIPLauncher::update);
+				jd.add(updater);
+				jd.setVisible(true);
+			} 
+				
 		} catch (Exception e) {
 			jd.setTitle("GoIP version cannot be confirmed.");
-		}
-
-		jd.setBounds(100, 100, 300, 100);
-		jd.setLocationRelativeTo(null);
-
-		if (currentVersion == version) {
-
-			jd.add(new JLabel("Choose your role:"), BorderLayout.NORTH);
-			JButton DMButton = new JButton("Dungeon Master");
-			DMButton.addActionListener(e -> {
-				try {
-					GoIPDM.main(args);
-					jd.dispose();
-				} catch (Exception d) {
-					jd.removeAll();
-					jd.add(new JLabel(d.getMessage()));
-					jd.revalidate();
-					jd.repaint();
-				}
-			});
-			jd.add(DMButton, BorderLayout.CENTER);
-
-			JButton PlayerButton = new JButton("Player");
-			PlayerButton.addActionListener(e -> {
-				try {
-					GoIPPlayer.main(args);
-					jd.dispose();
-				} catch (Exception d) {
-					jd.removeAll();
-					jd.add(new JLabel(d.getMessage()));
-					jd.revalidate();
-					jd.repaint();
-				}
-			});
-			jd.add(PlayerButton, BorderLayout.SOUTH);
-			jd.setVisible(true);
-		} else {
-			updater = new JButton("Update GoIP!");
-			updater.addActionListener(GoIPLauncher::update);
-			jd.add(updater);
-			jd.setVisible(true);
 		}
 	}
 
@@ -84,6 +83,7 @@ public class GoIPLauncher {
 		JFileChooser f = new JFileChooser();
 		f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		f.showSaveDialog(null);
+		
 
 		JLabel waitMessage = new JLabel("Please wait while it finishes");
 		jd.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -92,10 +92,9 @@ public class GoIPLauncher {
 		jd.add(waitMessage, BorderLayout.CENTER);
 		jd.revalidate();
 		jd.repaint();
-	
-		try {
-			download(f.getSelectedFile() + "/GoIP v" + currentVersion + ".jar");
 
+		try {
+			download(f.getSelectedFile() + "/GoIP v" + currentVersion/10 + ".jar");
 			jd.setTitle("Complete!");
 			jd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			jd.remove(waitMessage);
@@ -107,8 +106,7 @@ public class GoIPLauncher {
 			jd.setTitle("Sorry - cannot write file");
 			jd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			jd.remove(waitMessage);
-			jd.add(new JLabel("Unable to update."),
-					BorderLayout.CENTER);
+			jd.add(new JLabel("Unable to update."), BorderLayout.CENTER);
 			jd.revalidate();
 			jd.repaint();
 		}
@@ -116,9 +114,11 @@ public class GoIPLauncher {
 	}
 
 	private static void download(String target) throws IOException {
-		URL website = new URL("https://dl.dropboxusercontent.com/u/11902673/GoIP%20Launcher.jar");
+		URL website = new URL(
+				"https://dl.dropboxusercontent.com/u/11902673/GoIP%20Launcher.jar");
 		Path jar = Paths.get(target);
-		Files.copy(website.openStream(), jar, StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(website.openStream(), jar,
+				StandardCopyOption.REPLACE_EXISTING);
 	}
 
 	private static int getCurrentVersion() {
@@ -138,25 +138,6 @@ public class GoIPLauncher {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return version;
-	}
-
-	private static int getVersion() throws IOException {
-		File file;
-		int version = -1;
-		try {
-			file = new File(
-					"/Users/justinrisch/documents/workspace/GoIP/bin/version.txt");
-			Scanner in = new Scanner(file);
-			if (in.hasNextInt()) {
-				version = in.nextInt();
-			}
-			in.close();
-		} catch (Exception e) {
-			version = GoIPLauncher.class
-					.getResourceAsStream("/bin/version.txt").read();
-		}
-
 		return version;
 	}
 
